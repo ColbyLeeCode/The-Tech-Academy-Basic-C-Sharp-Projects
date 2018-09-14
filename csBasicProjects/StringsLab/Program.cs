787e4428b9f661bc4193807d4ce709ec626f801e
Colby <colby.lee@live.com>
2018-09-13 18:47:57 -0700

    added strings lab

---
+++ csBasicProjects/StringsLab/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Strings lab for The Tech Academy*/
+
+using System;
+using System.Text;
+
+
+namespace StringsLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "My name is ", s2 = "Bob ", s3 = "LeFour";
+            string total = s1 + s2 + s3;
+
+            Console.WriteLine(total);
+
+            Console.WriteLine(total.ToUpper());
+
+            int count = 0;
+            StringBuilder paragraph = new StringBuilder();
+            while (count < 5)
+            {
+                Console.WriteLine("Next sentence please: ");
+                paragraph.Append(Console.ReadLine() + " ");
+                count++;
+            }
+
+            Console.WriteLine(paragraph);
+            Console.Read();
+        }
+    }
+}
