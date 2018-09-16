da487f1178b1cdb8c2773fce8fa79b9eebfa11ed
Colby <colby.lee@live.com>
2018-09-16 14:58:10 -0700

    added classes lab 111

---
+++ csBasicProjects/ClassesLab111/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLab111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number would you like to perfrom calculations on? ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(myClass.addTwo(input));
+            Console.WriteLine(myClass.subTwo(input));
+            Console.WriteLine(myClass.divTwo(input));
+
+            Console.Read();
+        }
+    }
+}
---
+++ csBasicProjects/ClassesLab111/myClass.cs
@@ -0,0 +1,28 @@
+﻿/*Create a class. In that class, create three methods, each of which will take one 
+ * integer parameter in and return an integer. The methods should do some math 
+ * operation on the received parameter. Put this class in a separate .cs file in 
+ * the application.*/
+
+
+using System;
+
+namespace ClassesLab111
+{
+    class myClass
+    {
+        static public int addTwo(int input)
+        {
+            return input + 2;
+        }
+
+        static public int subTwo(int input)
+        {
+            return input - 2;
+        }
+
+        static public int divTwo(int input)
+        {
+            return input / 2;
+        }
+    }
+}
