2571241571277c79e1d7c68ebeefa5693a26a167
Colby <colby.lee@live.com>
2018-09-16 16:19:02 -0700

    added classes/optional parameters lab

---
+++ csBasicProjects/ClassesLabStep115/MyClass.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassesLab115
+{
+    class MyClass
+    {
+        public MyClass()
+        {
+        }
+
+        /*takes two integers as parameters. Make one of them optional. Have the method do 
+         * a math operation and return a integer result.*/
+        public int myMethod(int x, int y = 4)
+        {
+            return x + y;
+        }
+    }
+}
+ 
---
+++ csBasicProjects/ClassesLabStep115/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLab115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+
+            /*Ask the user to input two numbers, one at a time. Let them know they need 
+             * not enter anything for the second number.*/
+            Console.WriteLine("Please input at least one whole number to be used as a parameter. The second input value may be blank.");
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+
+            /*Call the method in MyClass, passing in the one or two numbers entered.*/
+            int outcome;
+            if (String.IsNullOrWhiteSpace(second))
+                outcome = myClass.myMethod(Convert.ToInt32(first));
+            else
+                outcome = myClass.myMethod(Convert.ToInt32(first), Convert.ToInt32(second));
+
+            Console.WriteLine("The returned vale of the method with optional parameter: " + outcome); ;
+        }
+
+        
+    }
+}
