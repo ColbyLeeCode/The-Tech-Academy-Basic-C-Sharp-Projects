5eddfd8fd222e66ebb570de060cda069dd7bceb0
Colby <colby.lee@live.com>
2018-09-16 15:54:56 -0700

    Added classes and overloaded methods lab 114

---
+++ csBasicProjects/ClassesLab114/MyClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassesLab114
+{
+    class MyClass
+    {
+        public MyClass()
+        {
+
+        }
+
+        /*will take in an integer, do a math operation to it and then return the answer as an integer.*/
+        public int myMethod(int num)
+        {
+            return num * 2;
+        }
+
+
+        /*will take in a decimal, do a different math operation to it and then return the answer as an integer.*/
+        public int myMethod(decimal d)
+        {
+            return Convert.ToInt32(d % 5);
+        }
+
+        /*call the third method, passing in a string that equates to an integer.*/
+        public int myMethod(string s)
+        {
+            int final = Convert.ToInt32(s) + 1000;
+            return final;
+        }
+    }
+
+
+}
+ 
+ 
+ 
