1fab6021f5d9d81a7890da52269dabc5e5265fd7
Colby <colby.lee@live.com>
2018-09-16 19:38:10 -0700

    Added abstraction step 124

---
+++ csBasicProjects/AbsractionStep124/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AbsractionStep124
+{
+    class Employee : Person
+    {
+        
+        int Id = 0;
+
+        //Calls the base constructor (Person)
+        public Employee(string FirstName, string LastName) : base(FirstName, LastName)
+        {
+            
+        }
+
+        //Override the  
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + "[" + base.FirstName + " " + base.LastName + "]");
+        }
+    }
+}
---
+++ csBasicProjects/AbsractionStep124/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AbsractionStep124
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee("Sample", "Student");
+            emp.SayName();
+
+            Console.Read();
+        }
+    }
+}
