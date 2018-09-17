2422316d0f18427e5d28885c19ed77ea4bcd2b29
Colby <colby.lee@live.com>
2018-09-17 16:10:34 -0700

    added structs step 137

---
+++ csBasicProjects/StructsStep137/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Structs Step 137*/
+using System;
+
+
+namespace StructsStep137
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number();
+            num.Amount = 1.34543m;
+
+            Console.WriteLine("Amount: " + num.Amount);
+            Console.Read();
+        }
+    }
+}
