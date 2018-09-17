1b4caae4083c990081c14b3e3ebf565eb14ad163
Colby <colby.lee@live.com>
2018-09-16 22:22:30 -0700

    added Interface step 126

---
+++ csBasicProjects/InterfacesStep126/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace InterfacesStep126
+{
+ 
+        class Employee : Person, IQuittable
+        {
+
+            int Id = 0;
+
+            //Calls the base constructor (Person)
+            public Employee(string FirstName, string LastName) : base(FirstName, LastName)
+            {
+
+            }
+
+        public void Quit()
+        {
+            Console.WriteLine("Press Enter to Exit... BYE!");
+            Console.Read();
+            Environment.Exit(0);
+        }
+
+        //Override base method 
+        public override void SayName()
+            {
+                Console.WriteLine("Name: " + "[" + base.FirstName + " " + base.LastName + "]");
+            }
+        }
+}
---
+++ csBasicProjects/InterfacesStep126/IQuittable.cs
@@ -0,0 +1,9 @@
+﻿/*Create an interface called IQuittable and have it define a void method called Quit().*/
+
+namespace InterfacesStep126
+{
+    interface IQuittable
+    {
+        void Quit();
+    }
+}
