e3a0ffa9c4cb936704a9cde0fc120d55521100f5
Colby <colby.lee@live.com>
2018-09-17 15:50:54 -0700

    added enums step 134

---
+++ csBasicProjects/EnumsStep134/Program.cs
@@ -0,0 +1,54 @@
+﻿/*Enums step 134 for The Tech Academy*/
+
+using System;
+using System.Linq;
+
+namespace EnumsStep134
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DaysOfWeek day = getInput();
+
+            Console.WriteLine("Day of week: " + day);
+            Console.ReadLine();
+        }
+
+        public static DaysOfWeek getInput()
+        {
+            DaysOfWeek input;
+
+            Start:
+            try
+            {
+
+                Console.WriteLine("Please select a day of the week: ");
+                string rawInput = Console.ReadLine();
+                if (rawInput.All(char.IsDigit))
+                    throw new Exception();
+                input = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), rawInput);
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                goto Start;
+            }
+
+            return input;
+        }
+    }
+
+    enum DaysOfWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
