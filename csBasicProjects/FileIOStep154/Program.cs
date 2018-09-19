e4edd05029cd4a942c283f7e0ab70b5c7aaa4258
Colby <colby.lee@live.com>
2018-09-18 17:40:45 -0700

    added file io step 154

---
+++ csBasicProjects/FileIOStep154/Program.cs
@@ -0,0 +1,33 @@
+﻿/*File IO step 154 for The Tech Academy*/
+
+using System;
+using System.IO;
+
+
+namespace FileIOStep154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number here: ");
+            string input = Console.ReadLine();
+
+            using (StreamWriter file = new StreamWriter(@"sample.txt", true))
+            {
+                //write the line to the text file
+                file.Write(input);
+            }
+
+            using (StreamReader file = new StreamReader("sample.txt"))
+            {
+                //read the text file back to the user
+                Console.WriteLine("File Contents: ");
+                Console.WriteLine(file.ReadToEnd());
+            }
+
+            Console.Read();
+
+        }
+    }
+}
