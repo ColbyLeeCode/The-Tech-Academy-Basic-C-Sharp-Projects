7d17b8cb9e042a838f801743d834d0266d427841
Colby <colby.lee@live.com>
2018-09-19 14:53:03 -0700

    adding constructor and variable exercise

---
+++ csBasicProjects/ConstructorChainingStep162/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Constructor and variable excercise for The Tech Academy, Step 162*/
+using System;
+
+
+namespace ConstructorChainingStep162
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string constString = "This is constant.";
+            Console.WriteLine(constString);
+            var num = 3;
+            Console.WriteLine("variable num is of type: " + num.GetType());
+            var str = "Is a string.";
+            Console.WriteLine("variable str is of type: " + str.GetType());
+
+            Person person = new Person();
+            person.SayName();
+            Console.ReadLine();
+
+        }
+    }
+}
