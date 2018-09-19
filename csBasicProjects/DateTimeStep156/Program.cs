72526d0c651335e86c497f0f8a5436eb9aa81d8f
Colby <colby.lee@live.com>
2018-09-18 19:38:55 -0700

    added date time step 156

---
+++ csBasicProjects/DateTimeStep156/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Date Time step 156 for The Tech Academy*/
+using System;
+
+namespace DateTimeStep156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Please input a number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            DateTime now = DateTime.Now;
+            DateTime later = now.AddHours(input);
+            Console.WriteLine("Time in {0} hours: {1}", input, later.TimeOfDay);
+            Console.Read();
+        }
+    }
+}
